e56e7e0d93e038d50b627741208e7f88b4178073
beat <beat3618@gmail.com>
2025-01-08 23:45:42 +0900

    都道府県一覧機能　CSV出力実装完了

---
+++ src/app/constants/file/prefectures.ts
@@ -0,0 +1,17 @@
+const PREFECTURE_CSV_HEADERS = [
+  'ID',
+  '都道府県名',
+  '地域',
+  '都道府県庁所在地',
+  '面積',
+  '人口',
+  '画像'
+]
+
+const PREFECTURE_CSV_FILE_NAME = '都道府県一覧'
+
+
+export {
+  PREFECTURE_CSV_HEADERS,
+  PREFECTURE_CSV_FILE_NAME,
+}
