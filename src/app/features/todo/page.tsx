d93bc13b316cf1080acbdce3dd1f308ccecb6478
beat <beat3618@gmail.com>
2025-02-23 20:52:03 +0900

    todoリスト　loading処理修正

--- src/app/features/todo/page.tsx
+++ src/app/features/todo/page.tsx
@@ -2,14 +2,11 @@
 
 import TodoList from './components/TodoList'
 import './styles/todo.css'
-import Loading from '../../components/common/Loading'
 import { useTodo } from './hooks/useTodo'
 
 // todo画面コンポーネント
 export default function TodoPage() {
-  const { todos, setTodos, isLoading } = useTodo()
-
-  if (isLoading) return <Loading />
+  const { todos, setTodos } = useTodo()
 
   return (
     <div>
